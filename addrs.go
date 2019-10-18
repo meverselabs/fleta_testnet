6cb1a98071c2e9955f2bda8106c1d13030907d5e
Wody <wody@firstchain.co>
2019-10-18 16:27:05 +0900

    more test account

---
+++ addrs.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/fletaio/fleta_testnet/common"
+
+var Addrs = []common.Address{}
+
+func init() {
+	for i := 0; i < 40000; i++ {
+		Addrs = append(Addrs, common.NewAddress(0, uint16(i+21000), 0))
+		Addrs = append(Addrs, common.NewAddress(1, uint16(i+21000), 0))
+	}
+}
